49900f33400eb862995d5aad4e386a61e53154b0
rhlalchand <ryanlalchand@gmail.com>
2021-10-18 17:12:31 -0400

    added basic pages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+<html>
+    <nav>
+        <a href="/scanner.js">Scan QR Code</a>
+        <a href="/registration.js">Register a Student</a>
+        <a href="/admin.js">Track Students - Admin only</a>
+    </nav>
+</html>
