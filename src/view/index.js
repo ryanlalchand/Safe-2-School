b86dd3c51318601d789ef9fe394765d1edc15405
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2021-11-08 14:40:03 -0500

    added new files

--- src/view/index.js
+++ src/view/index.js
@@ -1,7 +1,3 @@
 <html>
-    <nav>
-        <a href="/scanner.js">Scan QR Code</a>
-        <a href="/registration.js">Register a Student</a>
-        <a href="/admin.js">Track Students - Admin only</a>
-    </nav>
+    
 </html>
--- src/view/registration.js
+++ src/view/registration.js
@@ -1,13 +1,58 @@
 //register a student form
 
-<html>
-    <body>
-        <form>
-            <input type="text" name="first_name">First Name:</input>
-            <input type="text" name="last_name">Last Name:</input>
-            <input type="text" name="phone">Parent Phone Number: will need regex validation</input>
-            <button type="submit">Submit</button>
+//import packages needed
+const QRCode = require('qrcode')
+const express = require('express')
+const app = express();
+const bp = require('body-parser')
 
-        </form>
-    </body>
-</html>
+app.set("view engine", "ejs");
+app.use(bp.urlencoded({extended: false}));
+app.use(bp.json());
+
+//routing to app.ejs file
+app.get("/",(req, res) =>{
+    res.render("app");
+});
+
+//POST request listener to convert text/URL to QRcode
+app.post("/scan", (req, res) =>{
+    const url = req.body.url;
+
+//if input is null give error
+if (url.length === 0) res.send("Error: No data");
+
+
+QRCode.toDataURL(url,(err, src)=>{
+if (err) res.send("Error occurred");
+
+res.render("scan",{ src });
+    });
+});
+
+
+//set up port
+const port = 8085;
+app.listen(port, () => console.log("Server at port", port));
+
+
+//set up data for students (needs setters and getters for actual students)
+let data = {
+    name: "Jane Doe",
+    phnumber:"888888888"
+}
+
+//converting into data 
+let stringdata = JSON.stringify(data)
+
+//testing purposes
+QRCode.toString(stringdata,{type:'terminal'}, function(err,url){
+if(err) return console.log("error occured")
+console.log(url)
+})
+
+//getting base64 URL
+QRCode.toDataURL(stringdata, function(err, url){
+    if(err) return console.log("error occured")
+    console.log(url)
+})
