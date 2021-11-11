f62ffb1003b04635f1613b7b9ffbcc6f054482f9
AlyssaG565 <90293939+AlyssaG565@users.noreply.github.com>
2021-11-10 20:09:10 -0500

    update for trying to get qr code to work

--- src/view/index.js
+++
@@ -1,7 +0,0 @@
-<html>
-    <nav>
-        <a href="/scanner.js">Scan QR Code</a>
-        <a href="/registration.js">Register a Student</a>
-        <a href="/admin.js">Track Students - Admin only</a>
-    </nav>
-</html>
--- src/view/registration.js
+++ src/view/registration.js
@@ -6,13 +6,15 @@ const express = require('express')
 const app = express();
 const bp = require('body-parser')
 
-app.set("view engine", "ejs");
-app.use(bp.urlencoded({extended: false}));
+app.set('views','./views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
 app.use(bp.json());
 
-//routing to app.ejs file
+//routing to registration.ejs file
 app.get("/",(req, res) =>{
-    res.render("app");
+    res.render("index");
 });
 
 //POST request listener to convert text/URL to QRcode
@@ -28,11 +30,11 @@ if (err) res.send("Error occurred");
 
 res.render("scan",{ src });
     });
-});
+}); 
 
 
 //set up port
-const port = 5000;
+const port = 8085;
 app.listen(port, () => console.log("Server at port 5000"));
 
 
